34a1a8aac02328e63a4832fc2154ad7680708d2b
erict <erict@MSI>
2023-03-17 20:45:23 +0800

    加入專案檔案。

---
+++ 質數辦別2/質數辦別2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n, i;
+    int num = 0;
+    cin >> n;
+    for (i = 1; i <= n; i++) {
+     
+        if (n % i == 0)
+            num++;
+
+    }
+    if (num == 2) {
+        cout << "YES"<<endl;
+    }
+    else{
+        cout << "NO"<<endl;
+        }
+
+    return 0;
+
+}
